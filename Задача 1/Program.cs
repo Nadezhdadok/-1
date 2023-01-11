f79f15fe2d1e3c0406cc7b59f879c47e8ba927d4
homeworkdome <homeworkdome@outlook.com>
2023-01-11 22:41:16 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите предложение без знаков препинания");
+string str = Console.ReadLine();
+string[] strArray = str.Split();
+string max = strArray[0];
+foreach (string s in strArray)
+{
+    if (s.Length>max.Length)
+    {
+        max= s;
+    }
+}
+Console.WriteLine("Самое длинное слово в предложении:");
+Console.WriteLine(max);
